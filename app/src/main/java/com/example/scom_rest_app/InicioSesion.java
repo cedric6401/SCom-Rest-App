ed98bbe9b981804e7d7af6a0e71c753974452a02
cedric6401 <rashellfernandez6401@gmail.com>
2022-09-30 23:09:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/scom_rest_app/InicioSesion.java
@@ -0,0 +1,12 @@
+package com.example.scom_rest_app;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_sesion);
+    }
+}
